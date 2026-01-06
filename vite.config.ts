1b9af45e4ab8436061267015d7dda953f0f87952
krishna-aasatech <kirshnaraj.r@aasa.tech>
2026-01-06 14:40:43 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
-import path from 'path';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-// https://vite.dev/config/
-export default defineConfig({
-  define: {
-    __APP_ENV__: JSON.stringify(process.env.VITE_APP_NAME)
-  },
-  plugins: [react(), tailwindcss(),],
-    resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+  const appName = env.VITE_APP_NAME ?? "POS Dashboard";
+
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(appName),
     },
-  },
-})
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+  };
+});
